f5d6e96a0917f8516ba1fbc46065e58e4532f51d
Luke Wheldale <lukewheldale@gmail.com>
2024-02-14 17:00:23 +1030

    changed function back to be more clear

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // display form button function
-function showForm() {
-	document.getElementById("new-book").addEventListener("click", displayForm);
-	function displayForm() {
-		document.getElementById("book-form").style.display = "flex";
-	}
+document.getElementById("new-book").addEventListener("click", displayForm);
+function displayForm() {
+	document.getElementById("book-form").style.display = "flex";
 }
-showForm();
 
 // Array for storing Book objects
 const myLibrary = [];
